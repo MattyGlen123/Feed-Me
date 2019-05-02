52033ea8c6c4390f68e9d1bf901c419565be36e0
Matty <matty@Mattys.local>
2019-05-02 15:37:13 +0100

    added recipe modal

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -5,15 +5,15 @@ const createItem = (ingredient) => `<li class="">${ingredient}</li>`;
 export const renderDetails = (recipe) => {
   
   const markup = `
-    <img class="recipe-details__img" src="${recipe.img}" alt="${recipe.title}">
-    <h3 class="">${recipe.title}</h3>
-    <ul class="">
+    <span class="modal-return"><i class="fas fa-arrow-left"></i></span>
+    <div class="modal-image" style="background-image: url('${recipe.img}')"></div>
+    <h2 class="modal-title">${recipe.title}</h2>
+    <ul class="modal-list">
       ${recipe.ingredients.map(ingredient => createItem(ingredient)).join('')}
     </ul>
-    <a href="${recipe.url}"><p>${recipe.author}</p></a>
   `;
-  console.log(markup, recipe);
-    DOMelements.recipeDetails.insertAdjacentHTML('afterbegin', markup);
+  
+  DOMelements.modal.insertAdjacentHTML('afterbegin', markup);
 }
 
-export const clearDetails = () => DOMelements.recipeDetails.innerHTML = "";
+export const clearDetails = () => DOMelements.modal.innerHTML = "";
